f3490c46e130b4b6bd072f7dd2fb9827366a0663
Lucie KHAMLACH <lkhamlac@e2r7p2.clusters.42paris.fr>
2023-01-26 15:01:14 +0100

    reste a faire assign et insert + main des reverse iteratoret on passe a mapmais vraiment jsp ce quil y a de drole dans tout ca

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include "iterator_traits.hpp"
 #include "vector_iterator.hpp"
 #include <iostream>
+#include <stdexcept>      // std::out_of_range
 
 
 #define RED     "\033[31m"      
@@ -34,20 +35,104 @@ int main()
     // for (it = fdp.begin(); it != fdp.end(); ++it)
     //     std::cout << BOLDWHITE << " " << *it;
  
-    ft::vector<int> marine; 
-    marine.push_back(13);
-    marine.push_back(14);
-    marine.push_back(15);
-    marine.push_back(16);
-    marine.push_back(17);
-    marine.push_back(19);
-    marine.push_back(18);
+    ft::vector<int> test; 
+    test.push_back(13);
+    test.push_back(14);
+    test.push_back(15);
+    test.push_back(16);
+    test.push_back(17);
+    test.push_back(19);
+    test.push_back(321);
+    test.push_back(23);
+    test.push_back(180);
 
-//    ft::vector<int>::iterator iter = marine.end() - 4;
-   // std::cout << "LOL --> " << *iter << std::endl;
+//    ft::vector<int>::iterator iter = test.end() - 4;
+//    std::cout << "LOL --> " << *iter << std::endl;
 
-   //std::cout << marine.back() << std::endl;
-    std::cout <<"at > " << marine.at(10) << std::endl;
-   
+//  //  std::cout <<"at > " << test.begin() << std::endl;
+  
+//        std::cout <<"at > " << test.at(2) << std::endl;
+  ft::vector<int>::iterator it = test.begin();
+    for (it = test.begin(); it != test.end(); ++it)
+        std::cout << BOLDWHITE << " " << *it ;
+    std::cout<< std::endl;
+
+
+    
+    std::cout << YELLOW << "test de back" << std::endl;
+        std::cout << BOLDWHITE << "back : " << test.back() << std::endl;
+
+    std::cout << YELLOW << "test de at" << std::endl;
+        std::cout <<BOLDWHITE <<  "at de 2 : " << test.at(2) << std::endl;
+
+    std::cout << YELLOW << "test de at avec et sans exception" << std::endl;
+	try
+	{
+		std::cout << BOLDWHITE << "at(2) : " <<test.at(2) << std::endl;
+		std::cout << BOLDWHITE << "at(4) : " <<test.at(4) << std::endl;
+		std::cout << BOLDWHITE << "at(16) donc exception : " <<test.at(16) << std::endl;
+	}
+    catch(const std::exception& e)
+	{
+		std::cerr << RED<< e.what() << std::endl;
+	}
+
+
+
+
+    std::cout << YELLOW << "test de front" << std::endl;
+        std::cout <<BOLDWHITE << "front :  " << test.front() << std::endl;
+
+    std::cout << YELLOW << "test de operator[]" << std::endl;
+        std::cout <<BOLDWHITE << "operator[] de 4:  " << test[4] << std::endl;
+
+    std::cout << YELLOW << "test de size()" << std::endl;
+        std::cout <<BOLDWHITE << "size():  " << test.size() << std::endl;
+    std::cout << YELLOW << "test de capacity()" << std::endl;
+        std::cout <<BOLDWHITE << "capacity():  " << test.capacity() << std::endl;
+    std::cout << YELLOW << "test de max_size()" << std::endl;
+        std::cout <<BOLDWHITE << "max_size():  " << test.max_size() << std::endl;  
+    
+
+    std::cout << YELLOW << "test de erase()" << std::endl;
+     ft::vector<int> myvector;
+  for (int i=1; i<=10; i++) myvector.push_back(i);
+    for (it = myvector.begin(); it != myvector.end(); ++it)
+        std::cout << BOLDWHITE << " " << *it;
+    std::cout<< std::endl;
+
+  // erase the 6th element
+  std::cout << "on supprime le 6eme element" << std::endl;
+  myvector.erase (myvector.begin()+5);
+
+  // erase the first 3 elements:
+  std::cout << "on supprime les trois premiers elements" << std::endl;
+  myvector.erase (myvector.begin(),myvector.begin() + 3);
+
+  std::cout << "myvector contient:";
+  for (unsigned i=0; i<myvector.size(); ++i)
+    std::cout << ' ' << myvector[i];
+  std::cout << '\n';
+
+    std::cout << YELLOW << "test de popback()" << std::endl;
+
+  myvector.pop_back();
+    for (unsigned i=0; i<myvector.size(); ++i)
+    std::cout << BOLDWHITE << ' ' << myvector[i];
+  std::cout << '\n';
+
+
+    std::cout << YELLOW << "test de resize() avec (5), (8, 100) et (12)" << std::endl;
+    myvector.resize(5);
+    myvector.resize(8, 100);
+    myvector.resize(12);
+    for (unsigned i=0; i<myvector.size(); ++i)
+    std::cout << BOLDWHITE << ' ' << myvector[i];
+
+    std::cout << '\n';
+    std::cout << YELLOW << "test de empty()" << std::endl;
+    std::cout <<BOLDWHITE << "empty():  " << myvector.empty() << std::endl;
+
+  return 0;
 
  }
--- vector.hpp
+++ vector.hpp
@@ -48,7 +48,7 @@ class vector
     //default 
     explicit vector(const allocator_type& alloc = allocator_type())
     {
-        this->_alloc = alloc;
+        this->_alloc = alloc; 
         this->_arr = _alloc.allocate(0);
         this->_size = 0;
         this->_capacity = 0;
@@ -70,11 +70,11 @@ class vector
     vector(const vector& copy)
     {
         this->_alloc = copy._alloc;
-        this->_arr = copy._arr;
         this->_size = copy._size;
-        this->_capacity = copy._capacity;
+        this->_capacity = copy._capacity; // isize?
+        this->_arr = _alloc.allocate(_capacity); //._arr; // check icic
         for (size_type i = 0; i < _size; i++)
-			this->_alloc.construct(&this->_arr[i], copy.val);    
+			this->_alloc.construct(&this->_arr[i], copy._arr[i]);    
     }
 
     // destructeurs
@@ -86,7 +86,13 @@ class vector
         }
         if (_arr != NULL)
             _alloc.deallocate(_arr, _capacity);
+        // for (size_type i = 0; i < size(); i++)
+		// 		_alloc.destroy(&_arr[i]);
+		// 	_alloc.deallocate(_arr, capacity());
     }
+
+   
+
 
     //operateurs assignations
     vector & operator=( const vector & copy )
@@ -115,7 +121,6 @@ class vector
     {
         return this->_arr[n];
     }
-
  
     reference front()
     {
@@ -141,14 +146,14 @@ class vector
     {
         if (n >= _size)
             throw std::out_of_range("error out of range");
-        return(this->_arr[n]);
+        return this->_arr[n];
     }
     
     const_reference at (size_type n) const
     {
         if (n >= _size)
             throw std::out_of_range("error out of range");
-        return(this->_arr[n]);
+        return this->_arr[n];
     }
 
     
@@ -163,15 +168,15 @@ class vector
 
     iterator end(void) 
     { 
-        return iterator(_arr + _size); 
+        return iterator(_arr + _size);
     }
 
-    const_iterator cbegin(void) 
+    const_iterator cbegin(void) const
     { 
         return const_iterator(_arr); 
     }
 
-    const_iterator cend(void) 
+    const_iterator cend(void) const
     { 
         return const_iterator(_arr + _size); 
     }
@@ -208,31 +213,167 @@ class vector
 
     void push_back(const T& x)
 	{
-		// if (_capacity == 0)
-		// 	reserve(1);
-		// else if (_size >= _capacity)
-		// 	reserve(_capacity * 2);
+		if (_capacity == 0)
+			reserve(1);
+		else if (_size >= _capacity)
+			reserve(_capacity * 2);
 		_alloc.construct(&_arr[_size], x);
 		_size++;
 	}
 
+
     //ASSIGN
+   
+    // template <class InputIterator>  
+    //void assign (InputIterator first, InputIterator last)
+    // {
+        
+    // }
+    // void assign (size_type n, const value_type& val)
+    // {
+        
+    // }
+
     //POP BACK
+    void pop_back()
+    {
+			iterator it = end() - 1;
+			erase(it);
+	}
     //INSERT
+
+
     //ERASE
+    /*Erases the specified elements from the container.
+1) Removes the element at pos.
+2) Removes the elements in the range [first, last).
+Invalidates iterators and references at or after the point of the erase, including the end() iterator.
+
+The iterator pos must be valid and dereferenceable. Thus the end() iterator (which is valid, but is not dereferenceable) cannot be used as a value for pos.
+
+The iterator first does not need to be dereferenceable if first == last: erasing an empty range is a no-op.
+
+*/
+    iterator erase (iterator position)
+    	{
+		size_type dist = position - begin();
+		for (size_type i = dist; i < _size - 1; i++)
+		{
+			_alloc.destroy(_arr + i);
+			_alloc.construct(_arr + i, *(_arr + i + 1));
+		}
+		_size--;
+		_alloc.destroy(_arr + _size - 1);
+		return (iterator(_arr + dist));
+	}
+    
+    iterator erase (iterator first, iterator last)   
+    {
+        while (first != last)
+				erase(--last);
+			return (last);
+    }
+
     //SWAP
+    void swap(vector& x)
+    {
+        pointer	tmp_arr = x._arr;
+        size_t	tmp_size = x._size;
+        size_t	capacity = x._capacity;
+        Alloc	tmp_alloc = x._alloc;
+
+        x._arr = _arr;
+        x._size = _size;
+        x._capacity = _capacity;
+        x._alloc = _alloc;
+
+        _arr = tmp_arr;
+        _size = tmp_size;
+        _capacity = capacity;
+        _alloc = tmp_alloc;
+    }
 
 /////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////
-//////CAPACITY
 
     //SIZE
-    //MAX SIZE
-    //RESIZE
-    //CAPACITY
-    //EMPTY
-    //RESERVE
+    size_type size() const
+    {
+        return this->_size;
+    }
 
+    //MAX SIZE
+    size_type max_size() const
+    {
+        return this->_alloc.max_size();
+    }
+
+    //CAPACITY
+    size_type capacity() const
+    {
+        return this->_capacity;
+    }
+
+    //EMPTY
+    bool empty() const
+    {
+        return (cbegin() == cend());
+    }
+
+
+    //RESIZE --> change le nombre delement dans le vector ac une nouvelle size de notre choix alors que reserve c capacity
+    // sachant que soit on resize plus petit que la taille actuel et ca supp les derniers elements
+    // soit on resize plus grand et ca fout des 0
+    // soit on resize plusgrand avec un chiffre passe en paramentre genre alue type et ca complete ac ce chiffre la au lieu de 0
+    void resize (size_type n, value_type val = value_type())
+    {
+        if (n >size())
+        {
+            reserve(std::max(_size * 2, n));
+            while(_size < n)
+            {
+                _alloc.construct(&_arr[_size], val);
+                _size++;
+            }
+            _size = n;
+        }
+        else if (n < size())
+			erase(begin() + n, end());
+		_size = n;
+    }
+
+
+    //RESERVE
+/*
+if no reallocations happen or if the type of the elements has either a non-throwing 
+move constructor or a copy constructor, there are no changes in the container in case of exception 
+(strong guarantee).
+Otherwise, the container is guaranteed to end in a valid state (basic guarantee).
+The function throws length_error if n is greater than max_size.
+*/
+    void reserve (size_type n)
+    {
+        if (n > _alloc.max_size())
+            throw std::length_error("n is greater than max_size");
+        if (capacity() < n)
+        {
+            // si n > a capacity, on reallocate en augmentant la capacity to n
+            // sinon pas de reallocate et capacity est pas affecte
+            vector reserv = *this;
+            size_type tmp_size = _size;
+            size_type tmp_capacity = _capacity;
+            pointer old_arr = _arr;
+            clear();
+            _capacity = n;
+            if (_capacity)
+                _arr = _alloc.allocate(_capacity);
+            _size = tmp_size;
+            for (size_type i = 0; i < _size; i++) {
+                _alloc.construct(&_arr[i], reserv[i]);
+            }
+            _alloc.deallocate(old_arr, tmp_capacity);
+        }
+    }
 };
 
 
